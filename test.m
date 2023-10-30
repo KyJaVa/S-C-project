124954f2dc7833c8e4779f00869b3d0d13c1d673
KianSajjadi <Kian.s.sajjadi@gmail.com>
2023-10-30 16:49:52 +1100

    Add test.m code

---
+++ test.m
@@ -0,0 +1,26 @@
+clear all;
+close all;
+f = figure();
+ax = axes();
+
+cam = webcam("/dev/video4");
+cam.Resolution = '1920x1080';
+x = 1;
+
+
+while true
+   current_frame = snapshot(cam);
+   current_frame_grayscale = rgb2gray(current_frame);
+
+   [cornerPoints, boardSize] = detectCheckerboardPoints(current_frame_grayscale);
+   if isempty(cornerPoints)
+    imshow(current_frame_grayscale)
+    disp('no checkerboard')
+   else
+    hold on;
+    current_frame_grayscale = insertMarker(current_frame_grayscale, cornerPoints, 'o', 'Size', 10);
+    imshow(current_frame_grayscale);
+    boardSize
+    hold off;
+   end
+end
